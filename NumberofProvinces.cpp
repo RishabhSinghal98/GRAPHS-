f43418258119a998c068529ba21a778929d2e49d
Rishabh Singhal <rishabhs@192.168.1.18>
2024-07-15 13:05:12 +0530

    Graph Series

---
+++ NumberofProvinces.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    void dfs(vector<int> adj[], vector<int>&vis,int node)
+    {
+        vis[node]=1;
+
+        for(auto it : adj[node])
+        {     
+            if(!vis[it] )
+            {
+                dfs(adj,vis,it);
+            }
+        }
+
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V=isConnected.size();
+        vector <int> adj[V];
+        vector <int> visited(V,0);
+        int count=0;
+
+       for(int i=0;i<V;i++)
+       {
+        for(int j=0;j<V;j++)
+        {
+            if(isConnected[i][j] == 1 && i != j) { 
+                adj[i].push_back(j); 
+                adj[j].push_back(i); 
+                }
+        }
+       }
+
+       for(int i=0;i<V;i++)
+       {
+           if(!visited[i])
+           {
+             count++; // this means we are going for traversal and each time we go for traversal that means we are encountering new province
+            dfs(adj,visited,i);
+           }
+       }
+
+     return count;
+        
+    }
+};
+
